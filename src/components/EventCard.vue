b2511fbc1f642403ccd1167772eef41e61f80e47
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2022-05-17 10:01:11 +0200

    Style EventCard component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,16 +1,38 @@
 <template>
-    <div class="bg-slate-400 m-4 rounded-lg p-4">
-        <div>{{ event.date }}</div>
-        <div>{{ event.title }}</div>
-        <div>{{ event.body }}</div>
+    <div class="event-card my-4 rounded-lg drop-shadow-lg p-4">
+        <div class="event-time">{{ event.date }}</div>
+        <div class="event-title">{{ event.title }}</div>
+        <div class="event-text">{{ event.body }}</div>
     </div>
 </template>
 
 <script setup>
 defineProps({
-    event: Object
+    event: Object,
 });
 </script>
 
 <style>
+.event-card {
+    background-color: #0f05a0;
+    font-family: "Inter";
+    font-style: normal;
+    font-size: 28px;
+    line-height: 36px;
+}
+
+.event-time {
+    font-weight: 900;
+    color: #eb5e00;
+}
+
+.event-title {
+    font-weight: 900;
+    color: #ffbfab;
+}
+
+.event-text {
+    font-weight: 500;
+    color: #ffbfab;
+}
 </style>
