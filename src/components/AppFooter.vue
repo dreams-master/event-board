60dd97788304ededcf96e4219252711f2e265aab
Ralph Sennhauser <ralph.sennhauser@gmail.com>
2022-05-17 09:31:11 +0200

    Add footer component

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,40 @@
+<template>
+    <footer class="flex justify-between items-center p-5 footer">
+        <div class="logo-seb"></div>
+        <div class="logo-opportunity"></div>
+        <div class="logo-sag"></div>
+    </footer>
+</template>
+
+<script>
+</script>
+
+<style>
+.footer {
+    background-color: #ffffff;
+}
+
+.logo-seb {
+    width: 230px;
+    height: 44px;
+    background-image: url(STZH_SEB_Logo.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+.logo-opportunity {
+    width: 296px;
+    height: 55px;
+    background-image: url(Opportunity.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+
+.logo-sag {
+    width: 273px;
+    height: 52px;
+    background-image: url(SAG_Logo_De.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+</style>
